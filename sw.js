1dc1bac46bd341595576f91dde9fcc7abf73951a
marlontanlop-star <marlon.tanlop@gmail.com>
2026-03-24 17:19:01 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,12 @@
-const CACHE_NAME = 'mandre-pwa-v1';
-const assets = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
-
-// Instalación y almacenamiento en caché básico
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assets);
-    })
-  );
-  self.skipWaiting();
+    self.skipWaiting();
 });
 
-// Activación y limpieza de caches viejos
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
-  return self.clients.claim();
+    event.waitUntil(self.clients.claim());
 });
 
-// Responder a peticiones
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
+    // Esto es lo mínimo que pide Chrome para dejarte instalar
+    event.respondWith(fetch(event.request));
 });
